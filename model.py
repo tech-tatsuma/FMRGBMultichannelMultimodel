aeb133204924a1514fc01d4c2b734dbb0f80610c
kyutech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-09-13 23:23:14 +0900

    apply for multitask

--- model.py
+++ model.py
@@ -1,38 +1,349 @@
-from torch import nn
+from torch import nn, einsum
+import torch
+import torch.nn.functional as f
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+from previvitmodel import Attention, PreNorm, FeedForward
+import torch.nn as nn
+from ops_dcnv3.modules.dcnv3 import DCNv3
 
-
-# 3D Convolutional Neural Network
+# 3dcnn
 class ConvNet3D(nn.Module):
-    def __init__(self):
-        super(ConvNet3D, self).__init__()
+    def __init__(self, in_channels=3, num_tasks=5, batch_size=20, image_size=56):
+        super(MultiTaskConvNet3D, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv3d(5, 16, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1)),
+            nn.Conv3d(in_channels, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.MaxPool3d((2, 2, 2)),
-            # Continue with more layers as needed...
+            nn.MaxPool3d(2),
         )
-        self.fc = None
 
-    def forward_features(self, x):
-        print("Input shape:", x.shape, "Type:", x.dtype)  # Add this line
+        # Compute the size of Linear layer input
+        self._to_linear = None
+        x = torch.randn(batch_size, in_channels, 32, image_size, image_size)  # Replace with your input shape
+        self.convs(x)
+
+        # Shared layers
+        self.shared_fc = nn.Sequential(
+            nn.Linear(self._to_linear, 128),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+        )
+
+        # Task-specific layers
+        self.task_fcs = nn.ModuleList([nn.Linear(64, 1) for _ in range(num_tasks)])
+
+    def convs(self, x):
         x = self.conv(x)
-        print("After conv shape:", x.shape, "Type:", x.dtype)  # And this line
-        return x.view(x.size(0), -1)
-        
-    def forward(self, x):
-        if self.fc is None:
-            # Get output shape of conv layers
-            out = self.forward_features(x)
-            out_shape = out.shape[-1]
-            print("Flattened output shape:", out_shape)
-            # Define fc layer with the obtained output shape
-            self.fc = nn.Sequential(
-                nn.Linear(out_shape, 128),
-                nn.ReLU(),
-                nn.Linear(128, 2)  # Single output for regression
-            ).to(x.device)
-        x = self.forward_features(x)
-        print("After forward_features shape:", x.shape, "Type:", x.dtype)  # And this line
-        x = self.fc(x)
-        print("After fc shape:", x.shape, "Type:", x.dtype)  # And this line
+        if self._to_linear is None:
+            self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]*x[0].shape[3]
         return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)
+        x = self.shared_fc(x)
+        
+        # Compute task-specific outputs
+        outputs = [task_fc(x) for task_fc in self.task_fcs]
+        
+        return outputs
+# convlstm
+class ConvLSTMCell(nn.Module):
+    def __init__(self, input_dim, hidden_dim, kernel_size, bias):
+        super(ConvLSTMCell, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.conv = nn.Conv2d(in_channels=input_dim + hidden_dim,
+                              out_channels=4 * hidden_dim,
+                              kernel_size=kernel_size,
+                              padding=(kernel_size[0] // 2, kernel_size[1] // 2),
+                              bias=bias)
+
+    def forward(self, input_tensor, cur_state):
+        h_cur, c_cur = cur_state
+        combined_conv = self.conv(torch.cat([input_tensor, h_cur], dim=1))
+        i, f, o, g = torch.sigmoid(combined_conv).chunk(4, dim=1)
+        c_next = f * c_cur + i * torch.tanh(g)
+        return o * torch.tanh(c_next), c_next
+
+    def init_hidden(self, batch_size, image_size):
+        return (torch.zeros(batch_size, self.hidden_dim, *image_size, device=self.conv.weight.device),
+                torch.zeros(batch_size, self.hidden_dim, *image_size, device=self.conv.weight.device))
+
+
+class ConvLSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, kernel_size, num_layers, bias=True, return_all_layers=False):
+        super(ConvLSTM, self).__init__()
+
+        self.num_layers = num_layers
+        self.return_all_layers = return_all_layers
+
+        if not isinstance(kernel_size, list): kernel_size = [kernel_size] * num_layers
+        if not isinstance(hidden_dim, list): hidden_dim = [hidden_dim] * num_layers
+
+        cell_list = []
+        for i in range(self.num_layers):
+            cur_input_dim = input_dim if i == 0 else hidden_dim[i-1]
+            cell_list.append(ConvLSTMCell(input_dim=cur_input_dim, hidden_dim=hidden_dim[i], kernel_size=kernel_size[i], bias=bias))
+
+        self.cell_list = nn.ModuleList(cell_list)
+
+    def forward(self, input_tensor, hidden_state=None):
+        b, seq_len, _, h, w = input_tensor.size()
+
+        if hidden_state is None:
+            hidden_state = [cell.init_hidden(b, (h, w)) for cell in self.cell_list]
+
+        layer_output_list, last_state_list = [], []
+        for layer_idx, cell in enumerate(self.cell_list):
+            h, c = hidden_state[layer_idx]
+            output_inner = []
+            for t in range(seq_len):
+                h, c = cell(input_tensor[:, t, :, :, :], (h, c))
+                output_inner.append(h)
+            layer_output = torch.stack(output_inner, dim=1)
+            layer_output_list.append(layer_output.permute(0, 2, 1, 3, 4))
+            last_state_list.append((h, c))
+            input_tensor = layer_output
+
+        if not self.return_all_layers:
+            layer_output_list, last_state_list = layer_output_list[-1:], last_state_list[-1:]
+
+        return layer_output_list, last_state_list
+
+
+class ConvLSTM_FC(ConvLSTM):
+    def __init__(self, num_tasks=5, *args, **kwargs):
+        super(ConvLSTM_FC, self).__init__(*args, **kwargs)
+        self.num_tasks = num_tasks
+        self.attention = nn.Sequential(
+            nn.Conv2d(32, 1, kernel_size=1),
+            nn.Sigmoid()
+        )
+        
+        self.dropout1 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.5)
+        
+        self.shared_fc = nn.Sequential(
+            nn.Linear(32, 128),
+            nn.ReLU(),
+            self.dropout1,
+            nn.Linear(128, 32),
+            nn.ReLU(),
+            self.dropout2
+        )
+        
+        self.task_fcs = nn.ModuleList([nn.Linear(32, 1) for _ in range(num_tasks)])
+
+    def forward(self, input_tensor, hidden_state=None):
+        layer_output_list, last_state_list = super(ConvLSTM_FC, self).forward(input_tensor, hidden_state=hidden_state)
+        output = layer_output_list[-1][:, -1, :, :, :]
+        
+        attention_map = self.attention(output)
+        output = (output * attention_map).sum(dim=[2, 3])
+        
+        output = output.reshape(output.size(0), -1)
+        shared_output = self.shared_fc(output)
+        
+        # Compute task-specific outputs
+        outputs = [task_fc(shared_output) for task_fc in self.task_fcs]
+        
+        return outputs
+
+# ViViT
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        self.norm = nn.LayerNorm(dim)
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return self.norm(x)
+
+
+  
+class ViViT(nn.Module):
+    def __init__(self, image_size, patch_size, num_classes, num_frames, dim = 192, depth = 4, heads = 3, pool = 'cls', in_channels = 3, dim_head = 64, dropout = 0.,
+                 emb_dropout = 0., scale_dim = 4, ):
+        super().__init__()
+        
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+
+        assert image_size % patch_size == 0, 'Image dimensions must be divisible by the patch size.'
+        num_patches = (image_size // patch_size) ** 2
+        patch_dim = in_channels * patch_size ** 2
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b t c (h p1) (w p2) -> b t (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
+            nn.Linear(patch_dim, 128),
+            nn.Softmax(),
+            nn.Dropout(0.25),
+            nn.Linear(128, dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_frames, num_patches + 1, dim))
+        self.space_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.space_transformer = Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout)
+
+        self.temporal_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.temporal_transformer = Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout)
+
+        self.dropout = nn.Dropout(emb_dropout)
+        self.pool = pool
+
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.to_patch_embedding(x)
+        b, t, n, _ = x.shape
+
+        cls_space_tokens = repeat(self.space_token, '() n d -> b t n d', b = b, t=t)
+        x = torch.cat((cls_space_tokens, x), dim=2)
+        # x += self.pos_embedding[:, :, :(n + 1)]
+        x += self.pos_embedding[:, :t, :(n + 1)]
+        x = self.dropout(x)
+
+        x = rearrange(x, 'b t n d -> (b t) n d')
+        x = self.space_transformer(x)
+        x = rearrange(x[:, 0], '(b t) ... -> b t ...', b=b)
+
+        cls_temporal_tokens = repeat(self.temporal_token, '() n d -> b n d', b=b)
+        x = torch.cat((cls_temporal_tokens, x), dim=1)
+
+        x = self.temporal_transformer(x)
+        
+
+        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+
+        return self.mlp_head(x)
+
+class MultiTaskViViT(ViViT):
+    def __init__(self, num_tasks=5, *args, **kwargs):
+        super(MultiTaskViViT, self).__init__(*args, **kwargs)
+        self.num_tasks = num_tasks
+
+        # Shared Layer
+        self.shared_mlp_head = nn.Sequential(
+            nn.LayerNorm(self.dim),
+        )
+
+        # Individual fully connected layers for each task
+        self.task_fcs = nn.ModuleList([nn.Linear(self.dim, 1) for _ in range(num_tasks)])
+
+    def forward(self, x):
+        x = super(ViViT, self).forward(x)  # Run the forward pass of the original ViViT model until the pooling layer
+        
+        shared_output = self.shared_mlp_head(x)  # Use the shared layers
+
+        # Compute task-specific outputs
+        outputs = [task_fc(shared_output) for task_fc in self.task_fcs]
+
+        return outputs  # Return list of outputs for each task
+
+class DCNConvLSTMCell(nn.Module):
+    def __init__(self, input_dim, hidden_dim, kernel_size, bias, group=4, offset_scale=1.0):
+        super(DCNConvLSTMCell, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        self.dcn = DCNv3(
+            channels=input_dim + hidden_dim,
+            kernel_size=kernel_size,
+            group=group,
+            offset_scale=offset_scale
+        )
+
+        self.conv = nn.Conv2d(in_channels=hidden_dim,
+                              out_channels=4 * hidden_dim,
+                              kernel_size=1,
+                              padding=0,
+                              bias=bias)
+
+    def forward(self, input_tensor, cur_state):
+        h_cur, c_cur = cur_state
+        dcn_output = self.dcn(torch.cat([input_tensor, h_cur], dim=1))
+        combined_conv = self.conv(dcn_output)
+        i, f, o, g = torch.sigmoid(combined_conv).chunk(4, dim=1)
+        c_next = f * c_cur + i * torch.tanh(g)
+        return o * torch.tanh(c_next), c_next
+
+    def init_hidden(self, batch_size, image_size):
+        return (torch.zeros(batch_size, self.hidden_dim, *image_size, device=self.conv.weight.device),
+                torch.zeros(batch_size, self.hidden_dim, *image_size, device=self.conv.weight.device))
+
+class DCNConvLSTM(ConvLSTM):
+    def __init__(self, *args, **kwargs):
+        super(DCNConvLSTM, self).__init__(*args, **kwargs)
+
+    def forward(self, input_tensor, hidden_state=None):
+        b, seq_len, _, h, w = input_tensor.size()
+
+        if hidden_state is None:
+            hidden_state = [cell.init_hidden(b, (h, w)) for cell in self.cell_list]
+
+        layer_output_list, last_state_list = [], []
+        for layer_idx, cell in enumerate(self.cell_list):
+            h, c = hidden_state[layer_idx]
+            output_inner = []
+            for t in range(seq_len):
+                h, c = cell(input_tensor[:, t, :, :, :], (h, c))
+                output_inner.append(h)
+            layer_output = torch.stack(output_inner, dim=1)
+            layer_output_list.append(layer_output.permute(0, 2, 1, 3, 4))
+            last_state_list.append((h, c))
+            input_tensor = layer_output
+
+        if not self.return_all_layers:
+            layer_output_list, last_state_list = layer_output_list[-1:], last_state_list[-1:]
+
+        return layer_output_list, last_state_list
+
+class DCNConvLSTM_FC(DCNConvLSTM):
+    def __init__(self, num_tasks=5, *args, **kwargs):
+        super(DCNConvLSTM_FC, self).__init__(*args, **kwargs)
+        self.num_tasks = num_tasks
+        self.attention = nn.Sequential(
+            nn.Conv2d(32, 1, kernel_size=1),
+            nn.Sigmoid()
+        )
+        
+        self.dropout1 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.5)
+        
+        self.shared_fc = nn.Sequential(
+            nn.Linear(32, 128),
+            nn.ReLU(),
+            self.dropout1,
+            nn.Linear(128, 32),
+            nn.ReLU(),
+            self.dropout2
+        )
+        
+        self.task_fcs = nn.ModuleList([nn.Linear(32, 1) for _ in range(num_tasks)])
+
+    def forward(self, input_tensor, hidden_state=None):
+        layer_output_list, last_state_list = super(DCNConvLSTM_FC, self).forward(input_tensor, hidden_state=hidden_state)
+        output = layer_output_list[-1][:, -1, :, :, :]
+        
+        attention_map = self.attention(output)
+        output = (output * attention_map).sum(dim=[2, 3])
+        
+        output = output.reshape(output.size(0), -1)
+        shared_output = self.shared_fc(output)
+        
+        # Compute task-specific outputs
+        outputs = [task_fc(shared_output) for task_fc in self.task_fcs]
+        
+        return outputs
--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 from dataset import VideoDataset
-from model import ConvNet3D
+from model import ConvNet3D, ConvLSTM_FC, MultiTaskViViT, DCNConvLSTM_FC
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split, Subset
 from torchvision import transforms
+from torch.optim.lr_scheduler import StepLR
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
@@ -11,117 +12,251 @@ import argparse
 import os
 import glob
 from torch import nn
+import numpy as np
+from torchinfo import summary
+import sys
+import datetime
+import random
 
+# シードの設定を行う関数
+def seed_everything(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+# メイン関数
 def train(opt):
+
+    # シードの設定
+    seed_everything(opt.seed)
+
+    # デバイスの設定
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
+    # オプションで入力された値を取得
     data_file = opt.data
     epochs = opt.epochs
-    test_size = opt.test_size
+    train_size = opt.train_size
     patience = opt.patience
-    # Create your transform
+    learningmethod = opt.learnmethod
+    learning_rate = opt.lr
+    usescheduler = opt.usescheduler
+
+    # データの前処理
     transform = transforms.Compose([
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) 
+        transforms.Resize((56, 56)), 
+        transforms.ToTensor()
     ])
 
-    # Load CSV file
-    # df = pd.read_csv(data_file)
-    file_list = glob.glob(os.path.join(opt.data, '**', '*.pt'), recursive=True)
-    train_files, test_files = train_test_split(file_list, test_size=opt.test_size, random_state=42)
-    train_files, val_files = train_test_split(train_files, test_size=opt.test_size, random_state=42)
+    # データセットの取得
+    full_dataset = VideoDataset(csv_file=data_file, transform=transform)
+    train_size = int(train_size * len(full_dataset))
+    val_size = len(full_dataset) - train_size
 
-    # Split the dataset into train and test
-    # train_df, val_df = train_test_split(df, test_size=test_size)
+    # データの分割
+    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
 
-    # Create your datasets
-    train_dataset = VideoDataset(train_files, transform=transform)
-    val_dataset = VideoDataset(val_files, transform=transform)
-    test_dataset = VideoDataset(test_files, transform=transform)
+    # データローダの取得
+    train_loader = DataLoader(train_dataset, batch_size=20, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=20, shuffle=False)
 
-    # Create your dataloaders
-    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
+    # モデルの選択
+    if learningmethod=='conv3d':
+        # conv3dモデルの設定
+        model = ConvNet3D(in_channels=3, num_tasks=5, batch_size=20, image_size=56).to(device)
+        criterion = nn.MSELoss()
 
-    # Create the model
-    model = ConvNet3D().to(device)
+    elif learningmethod=='convlstm':
+        # convlstmの設定
+        model = ConvLSTM_FC(input_dim=3, hidden_dim=[64, 32, 16], kernel_size=(3, 3), num_layers=3).to(device)
+        criterion = nn.MSELoss()
 
-    # Define a loss function and optimizer
-    criterion = nn.CrossEntropyLoss()  # Use MSE for regression
-    optimizer = torch.optim.Adam(model.parameters())
+    elif learningmethod=='vivit':
+        # vivitの設定
+        model = MultiTaskViViT(image_size=64, patch_size=16, num_classes=2, num_frames=64, in_channels=3).to(device)
+        criterion = nn.MSELoss()
 
-    # Initialize variables for Early Stopping
+    elif learningmethod=='convlstmwithdcn':
+        # DCNConvLSTM_FCの設定
+        model =  DCNConvLSTM_FC(input_dim=3, hidden_dim=[64, 32, 16], kernel_size=(3,3), num_layers=3).to(device)
+        criterion = nn.MSELoss()
+    else:
+        # 入力が不適切だった時の処理
+        print('error: inappropriate input(learning method)')
+        return
+
+    # 複数GPUの並行処理
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        model = nn.DataParallel(model)
+
+    # モデルをデバイスに転送
+    model.to(device)
+
+    # 最適化手法の設定と学習率の設定
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # 学習率スケジューラの設定
+    if usescheduler == 'true':
+        scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
+
+    # 早期終了のための変数を定義
     val_loss_min = None
     val_loss_min_epoch = 0
 
-    # Initialize lists to monitor train and validation losses
+    # 学習結果を監視するための配列を定義
     train_losses = []
     val_losses = []
 
-    # Train the model
-    for epoch in range(epochs):  # Number of epochs
-        train_loss = 0
-        val_loss = 0
+    # モデルの訓練
+    for epoch in tqdm(range(epochs)):
+
+        # 各種パラメータの初期化
+        train_loss = 0.0
+        val_loss = 0.0
+
+        # モデルをtrainモードに設定
         model.train()
-        for i, (inputs, labels) in tqdm(enumerate(train_loader, 0)):
+
+        # trainデータのロード
+        for i, (inputs, labels) in enumerate(train_loader):
+
+            # ビデオとラベルをGPUへ転送
             inputs, labels = inputs.to(device), labels.to(device)
-            # Zero the parameter gradients
+
+            # 勾配の初期化
             optimizer.zero_grad()
-            print(inputs.shape)
-            # Forward + backward + optimize
+
+            # データはfloat型でなければならない
             if inputs.dtype != torch.float32:
                 inputs = inputs.float()
+
+            # モデルの適用
             outputs = model(inputs)
+
+            # Forward + backward + optimize
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
+
             train_loss += loss.item()
 
         train_loss /= len(train_loader)
         train_losses.append(train_loss)
 
+        # モデルを評価モードの設定
         model.eval()
-        val_loss = 0
+        
+        # 検証データでの評価
         with torch.no_grad():
-            for i, (inputs, labels) in tqdm(enumerate(val_loader, 0)):
+            for i, (inputs, labels) in enumerate(val_loader):
                 inputs, labels = inputs.to(device), labels.to(device)
                 if inputs.dtype != torch.float32:
                     inputs = inputs.float()
+                    
                 outputs = model(inputs)
+
                 val_loss += criterion(outputs, labels).item()
+                
 
         val_loss /= len(val_loader)
         val_losses.append(val_loss)
 
         print(f'Epoch {epoch+1}, Validation loss: {val_loss:.4f}')
+        sys.stdout.flush()
 
-            # Save the model if validation loss decreases
+        # メモリーを最適化する
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
+        # バリデーションロスが下がった時は結果を保存する
         if val_loss_min is None or val_loss < val_loss_min:
-            torch.save(model.state_dict(), 'model.pt')
+            model_save_name = f'{learningmethod}_lr{learning_rate}_ep{epochs}_pa{patience}intweak.pt'
+            torch.save(model.state_dict(), model_save_name)
             val_loss_min = val_loss
             val_loss_min_epoch = epoch
-        # If the validation loss didn't decrease for 'patience' epochs, stop the training
+            
+        # もしバリデーションロスが一定期間下がらなかったらその時点で学習を終わらせる
         elif (epoch - val_loss_min_epoch) >= patience:
             print('Early stopping due to validation loss not improving for {} epochs'.format(patience))
             break
 
-    # Plotting the training progress
+        # 学習率スケジューラを適用する
+        if usescheduler == 'true':
+            scheduler.step()
+
+    # Plotting the training and validation progress
     plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
     plt.plot(train_losses, label='Training loss')
     plt.plot(val_losses, label='Validation loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
-    plt.savefig('training_validation_loss.png')
+
+    return train_loss, val_loss_min
 
 if __name__=='__main__':
+    # get start time of program
+    start_time = datetime.datetime.now()
+    print('start time:',start_time)
+    sys.stdout.flush()
+    # setting parser
     parser = argparse.ArgumentParser()
     parser.add_argument('--data',type=str, required=True, help='csv data')
     parser.add_argument('--epochs',type=int, required=True, help='epochs')
-    parser.add_argument('--test_size', type=float, required=True, default=0.2, help='testdata_ratio')
-    parser.add_argument('--patience', type=int, required=True, default=5, help='patience')
+    parser.add_argument('--lr',type=float, default=0.001, help='learning rate')
+    parser.add_argument('--train_size', type=float, default=0.2, help='testdata_ratio')
+    parser.add_argument('--patience', type=int, default=5, help='patience')
+    parser.add_argument('--learnmethod', type=str, default='conv3d', help='conv3d or convlstm or vivit')
+    parser.add_argument('--islearnrate_search', type=str, default='false', help='is learningrate search ?')
+    parser.add_argument('--usescheduler', type=str, default='false', help='use lr scheduler true or false')
+    parser.add_argument('--seed', type=int, default=42, help='Seed for random number generators')
     opt = parser.parse_args()
+    # confirm the option
     print(opt)
-    print('-----biginning training-----')
-    train(opt)
+    sys.stdout.flush()
+
+    if opt.islearnrate_search == 'false':
+        print('-----biginning training-----')
+        sys.stdout.flush()
+        train_loss, val_loss = train(opt)
+        print('final validation loss: ', val_loss)
+        sys.stdout.flush()
+
+    elif opt.islearnrate_search == 'true':
+        learning_rates = [0.001, 0.002, 0.003, 0.004]
+        best_loss = float('inf')
+        best_lr = 0
+        for lr in learning_rates:
+            opt.lr = lr
+            print(f"\nTraining with learning rate: {lr}")
+            sys.stdout.flush()
+            print('-----beginning training-----')
+            sys.stdout.flush()
+        
+            train_loss, val_loss = train(opt)
+        
+            if val_loss < best_loss:
+                best_loss = val_loss
+                best_lr = lr
+        print('best validation loss: ', best_loss)
+        sys.stdout.flush()
+        print(f"Best learning rate: {best_lr}")
+        sys.stdout.flush()
+
+    else:
+        print('error: inappropriate input(islearnrate_search)')
+    # get end time of program
+    end_time = datetime.datetime.now()
+    # calculate execution time with start and end time
+    execution_time = end_time - start_time
     print('-----completing training-----')
+    sys.stdout.flush()
+    print('end time:',end_time)
+    sys.stdout.flush()
+    print('Execution time: ', execution_time)
+    sys.stdout.flush()
